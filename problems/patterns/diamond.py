1ebc5d3657a12a8defe34178de3b512c2532768e
sheikshammadh <sheikshammadh18@gmail.com>
2025-02-19 23:04:33 +0530

    sn

---
+++ problems/patterns/diamond.py
@@ -0,0 +1,8 @@
+def diamond_pattern(n):
+    for i in range(n):
+        print(" " * (n - i - 1) + "* " * (i + 1))
+    for i in range(n - 1):
+        print(" " * (i + 1) + "* " * (n - i - 1))
+
+n = 5
+diamond_pattern(n)
