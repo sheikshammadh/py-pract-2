dc09bdc830291fdedccc5d89f435d081016cc864
sheikshammadh <sheikshammadh18@gmail.com>
2025-02-20 22:01:30 +0530

    sn

--- strings/code.py
+++ strings/code.py
@@ -1,20 +1,20 @@
-# a = "shyam"
-# b = a.upper()
-# print(b) SHYAM every character into capital
+a = "shyam"
+b = a.upper()
+print(b) #SHYAM every character into capital
 
-# a = "shyam"
-# b = a.capitalize()
-# print(b) Shyam first character into capital
+a = "shyam"
+b = a.capitalize()
+print(b) #Shyam first character into capital
 
-# a = "shyam"
-# b = a.title()
-# print(b) Shyam nothing but capitalize
+a = "shyam"
+b = a.title()
+print(b) #Shyam nothing but capitalize
 
-# a = "Shyam"
-# b = a.isupper()
-# print(b) 
+a = "Shyam"
+b = a.isupper()
+print(b) 
 
-# A="hi"
-# # B=A.join("abc")
-# print(A.join(abc))
+A="hi"
+# B=A.join("abc")
+print(A.join('abc'))
 
