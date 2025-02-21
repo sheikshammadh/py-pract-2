a66d42963a674d44cdfa744028bd99e66ddfc53e
sheikshammadh <sheikshammadh18@gmail.com>
2025-02-21 16:02:47 +0530

    sn

---
+++ end sap/code.py
@@ -0,0 +1,4 @@
+#for i in range(20):
+    #print(i)# it iterates all the numbers in line by line
+    #print(i,end='')#012345678910111213141516171819
+    
--- strings/code.py
+++ strings/code.py
@@ -1,20 +1,31 @@
-a = "shyam"
-b = a.upper()
-print(b) #SHYAM every character into capital
+# a = "shyam"
+# b = a.upper()
+# print(b) #SHYAM every character into capital
 
-a = "shyam"
-b = a.capitalize()
-print(b) #Shyam first character into capital
+# a = "shyam"
+# b = a.capitalize()
+# print(b) #Shyam first character into capital
 
-a = "shyam"
-b = a.title()
-print(b) #Shyam nothing but capitalize
+# a = "shyam"
+# b = a.title()
+# print(b) #Shyam nothing but capitalize
 
-a = "Shyam"
-b = a.isupper()
-print(b) 
+# a = "Shyam"
+# b = a.isupper()
+# print(b) 
 
-A="hi"
+# A="hi"
 # B=A.join("abc")
-print(A.join('abc'))
+# print(A.join(B))
+
+# a="hi"
+# b=a.endswith("i")
+# print(b)
+
+# a=[123,143,134,1234]
+# def shyam (nandu):
+#     return nandu*3
+# sana=list(map(shyam,a))
+# print(sana)
+
 
